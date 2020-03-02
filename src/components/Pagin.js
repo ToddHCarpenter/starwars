dc3d812b049f03b94486e4cfbac5f4062782d13b
carpenth <carpenth@mail.uc.edu>
2020-03-02 14:34:33 -0500

    style changes

--- src/components/Pagin.js
+++ src/components/Pagin.js
@@ -1,32 +1,27 @@
 import React from "react";
 
 //bootstrap
-import Pagination from 'react-bootstrap/Pagination';
+import Pagination from "react-bootstrap/Pagination";
 
-const Pagin = (props) => {
-
+const Pagin = props => {
   const style = {
-    margin: '0 auto',
-    width: '20%',
-    marginBottom: '20px'
-
-  }
+    diplay: "flex",
+    justifyContent: "center"
+  };
 
   return (
-
-    <Pagination style={style} >
-      <Pagination.Item onClick={(e) => props.nextPage(1, e)}>{1}</Pagination.Item>
-      <Pagination.Item onClick={(e) => props.nextPage(2, e)}>{2}</Pagination.Item>
-      <Pagination.Item onClick={(e) => props.nextPage(3, e)}>{3}</Pagination.Item>
-      <Pagination.Item onClick={(e) => props.nextPage(4, e)}>{4}</Pagination.Item>
-      <Pagination.Item onClick={(e) => props.nextPage(5, e)}>{5}</Pagination.Item>
-      <Pagination.Item onClick={(e) => props.nextPage(6, e)}>{6}</Pagination.Item>
-      <Pagination.Item onClick={(e) => props.nextPage(7, e)}>{7}</Pagination.Item>
-      <Pagination.Item onClick={(e) => props.nextPage(8, e)}>{8}</Pagination.Item>
-      <Pagination.Item onClick={(e) => props.nextPage(9, e)}>{9}</Pagination.Item>
+    <Pagination style={style}>
+      <Pagination.Item onClick={e => props.nextPage(1, e)}>{1}</Pagination.Item>
+      <Pagination.Item onClick={e => props.nextPage(2, e)}>{2}</Pagination.Item>
+      <Pagination.Item onClick={e => props.nextPage(3, e)}>{3}</Pagination.Item>
+      <Pagination.Item onClick={e => props.nextPage(4, e)}>{4}</Pagination.Item>
+      <Pagination.Item onClick={e => props.nextPage(5, e)}>{5}</Pagination.Item>
+      <Pagination.Item onClick={e => props.nextPage(6, e)}>{6}</Pagination.Item>
+      <Pagination.Item onClick={e => props.nextPage(7, e)}>{7}</Pagination.Item>
+      <Pagination.Item onClick={e => props.nextPage(8, e)}>{8}</Pagination.Item>
+      <Pagination.Item onClick={e => props.nextPage(9, e)}>{9}</Pagination.Item>
     </Pagination>
-
-  )
+  );
 };
 
 export default Pagin;
